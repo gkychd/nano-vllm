7011f4338545452f0d794aabc1e942a9fa117667
gky <gky@example.com>
2026-03-09 17:26:26 +0800

    qwen3.py bug fix

--- nanovllm/models/qwen3.py
+++ nanovllm/models/qwen3.py
@@ -56,7 +56,6 @@ class Qwen3Attention(nn.Module):
             rotary_dim=self.head_dim,
             max_position=max_position,
             base=rope_theta,
-            rope_scaling=rope_scaling,
         )
         self.attn = Attention(
             self.num_heads,
